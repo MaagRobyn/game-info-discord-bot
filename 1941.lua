e170297c0c13e835de276249b5ff80f548f5b8be
Robyn Maag <maag.matthew@outlook.com>
2023-12-12 16:28:23 -0800

    Initial commit for tabletop sim setup button

---
+++ 1941.lua
@@ -0,0 +1,501 @@
+AAGBag = getObjectFromGUID(Global.getVar('AAGBagID'));
+INDBag = getObjectFromGUID(Global.getVar('INDBagID'));
+CH_InfBag = getObjectFromGUID(Global.getVar('CH_InfBagID'));
+Token1Bag = getObjectFromGUID(Global.getVar('Token1BagID'));
+Token3Bag = getObjectFromGUID(Global.getVar('Token3BagID'));
+Token5Bag = getObjectFromGUID(Global.getVar('Token5BagID'));
+
+ITA_InfBag = getObjectFromGUID(Global.getVar('ITA_InfBagID'));
+ITA_ArtBag = getObjectFromGUID(Global.getVar('ITA_ArtBagID'));
+ITA_TanBag = getObjectFromGUID(Global.getVar('ITA_TanBagID'));
+ITA_FigBag = getObjectFromGUID(Global.getVar('ITA_FigBagID'));
+ITA_BomBag = getObjectFromGUID(Global.getVar('ITA_BomBagID'));
+ITA_SubBag = getObjectFromGUID(Global.getVar('ITA_SubBagID'));
+ITA_TraBag = getObjectFromGUID(Global.getVar('ITA_TraBagID'));
+ITA_DesBag = getObjectFromGUID(Global.getVar('ITA_DesBagID'));
+ITA_CruBag = getObjectFromGUID(Global.getVar('ITA_CruBagID'));
+ITA_AirBag = getObjectFromGUID(Global.getVar('ITA_AirBagID'));
+ITA_BatBag = getObjectFromGUID(Global.getVar('ITA_BatBagID'));
+JPN_InfBag = getObjectFromGUID(Global.getVar('JPN_InfBagID'));
+JPN_ArtBag = getObjectFromGUID(Global.getVar('JPN_ArtBagID'));
+JPN_TanBag = getObjectFromGUID(Global.getVar('JPN_TanBagID'));
+JPN_FigBag = getObjectFromGUID(Global.getVar('JPN_FigBagID'));
+JPN_BomBag = getObjectFromGUID(Global.getVar('JPN_BomBagID'));
+JPN_SubBag = getObjectFromGUID(Global.getVar('JPN_SubBagID'));
+JPN_TraBag = getObjectFromGUID(Global.getVar('JPN_TraBagID'));
+JPN_DesBag = getObjectFromGUID(Global.getVar('JPN_DesBagID'));
+JPN_CruBag = getObjectFromGUID(Global.getVar('JPN_CruBagID'));
+JPN_AirBag = getObjectFromGUID(Global.getVar('JPN_AirBagID'));
+JPN_BatBag = getObjectFromGUID(Global.getVar('JPN_BatBagID'));
+GER_InfBag = getObjectFromGUID(Global.getVar('GER_InfBagID'));
+GER_ArtBag = getObjectFromGUID(Global.getVar('GER_ArtBagID'));
+GER_TanBag = getObjectFromGUID(Global.getVar('GER_TanBagID'));
+GER_FigBag = getObjectFromGUID(Global.getVar('GER_FigBagID'));
+GER_BomBag = getObjectFromGUID(Global.getVar('GER_BomBagID'));
+GER_SubBag = getObjectFromGUID(Global.getVar('GER_SubBagID'));
+GER_TraBag = getObjectFromGUID(Global.getVar('GER_TraBagID'));
+GER_DesBag = getObjectFromGUID(Global.getVar('GER_DesBagID'));
+GER_CruBag = getObjectFromGUID(Global.getVar('GER_CruBagID'));
+GER_AirBag = getObjectFromGUID(Global.getVar('GER_AirBagID'));
+GER_BatBag = getObjectFromGUID(Global.getVar('GER_BatBagID'));
+SU_InfBag = getObjectFromGUID(Global.getVar('SU_InfBagID'));
+SU_ArtBag = getObjectFromGUID(Global.getVar('SU_ArtBagID'));
+SU_TanBag = getObjectFromGUID(Global.getVar('SU_TanBagID'));
+SU_FigBag = getObjectFromGUID(Global.getVar('SU_FigBagID'));
+SU_BomBag = getObjectFromGUID(Global.getVar('SU_BomBagID'));
+SU_SubBag = getObjectFromGUID(Global.getVar('SU_SubBagID'));
+SU_TraBag = getObjectFromGUID(Global.getVar('SU_TraBagID'));
+SU_DesBag = getObjectFromGUID(Global.getVar('SU_DesBagID'));
+SU_CruBag = getObjectFromGUID(Global.getVar('SU_CruBagID'));
+SU_AirBag = getObjectFromGUID(Global.getVar('SU_AirBagID'));
+SU_BatBag = getObjectFromGUID(Global.getVar('SU_BatBagID'));
+UK_InfBag = getObjectFromGUID(Global.getVar('UK_InfBagID'));
+UK_ArtBag = getObjectFromGUID(Global.getVar('UK_ArtBagID'));
+UK_TanBag = getObjectFromGUID(Global.getVar('UK_TanBagID'));
+UK_FigBag = getObjectFromGUID(Global.getVar('UK_FigBagID'));
+UK_BomBag = getObjectFromGUID(Global.getVar('UK_BomBagID'));
+UK_SubBag = getObjectFromGUID(Global.getVar('UK_SubBagID'));
+UK_TraBag = getObjectFromGUID(Global.getVar('UK_TraBagID'));
+UK_DesBag = getObjectFromGUID(Global.getVar('UK_DesBagID'));
+UK_CruBag = getObjectFromGUID(Global.getVar('UK_CruBagID'));
+UK_AirBag = getObjectFromGUID(Global.getVar('UK_AirBagID'));
+UK_BatBag = getObjectFromGUID(Global.getVar('UK_BatBagID'));
+US_InfBag = getObjectFromGUID(Global.getVar('US_InfBagID'));
+US_ArtBag = getObjectFromGUID(Global.getVar('US_ArtBagID'));
+US_TanBag = getObjectFromGUID(Global.getVar('US_TanBagID'));
+US_FigBag = getObjectFromGUID(Global.getVar('US_FigBagID'));
+US_BomBag = getObjectFromGUID(Global.getVar('US_BomBagID'));
+US_SubBag = getObjectFromGUID(Global.getVar('US_SubBagID'));
+US_TraBag = getObjectFromGUID(Global.getVar('US_TraBagID'));
+US_DesBag = getObjectFromGUID(Global.getVar('US_DesBagID'));
+US_CruBag = getObjectFromGUID(Global.getVar('US_CruBagID'));
+US_AirBag = getObjectFromGUID(Global.getVar('US_AirBagID'));
+US_BatBag = getObjectFromGUID(Global.getVar('US_BatBagID'));
+
+zOffset = 2
+
+--Italian Territories
+Italy = {-22,4}
+Balkans = {-18,3}
+Libya = {-19,-5}
+
+-- German Territories
+Morocco_Algeria = {-25,-2}
+France = {-25,7}
+NW_Europe = {-23,10}
+Germany = {-20,9}
+Czechoslovakia_Hungary = {-17,6}
+Poland = {-15,11}
+Norway = {-21,17}
+Finland = {-15,18}
+Bulgaria_Romania = {-15,3}
+
+-- Japanese Territories
+French_IndoChina_Thailand = {9,-9}
+Kiangsu = {13,5}
+Manchuria = {17,10}
+Japan = {23,6}
+Formosa = {14.5,-4}
+Okinawa = {21.5,-1.5}
+Iwo_Jima = {26.5,2}
+Caroline_Islands = {24,-8.5}
+
+-- Soviet Territories
+Baltic_States = {-14,15}
+Karrelia_SSR = {-13,19}
+E_Poland = {-13,11}
+Belorussia = {-10,13}
+Ukraine = {-11,6}
+E_Ukraine = {-9,9}
+Caucasus = {-8,5}
+Archangel = {-5,18}
+Russia = {-5,12}
+Kazakh_SSR = {-1,3}
+Novosibirsk = {0,11}
+Evenki_National_Okrug = {6,18}
+Yakut_SSR = {12,21}
+Stanovoj_Chrebet = {15.5,17}
+Buryatia_SSR = {20,15}
+Soviet_Far_East = {23,20}
+
+-- UK Territories
+UK = {-25,12}
+W_Canada = {42,16}
+Kwangtung = {12,-4}
+Australia = {21,-21}
+India = {0,-7}
+TransJordan = {-11,-3} -- Good for Jordan
+Union_of_S_Africa = {-17,-23}
+Egypt = {-15,-6}
+E_Canada = {-37,13}
+Burma = {7,-7}
+
+-- US Territories
+E_US = {-41.5,5}
+Alaska = {36,19}
+C_US = {43,7}
+W_US = {41,7}
+Midway = {33,3.5}
+Wake_Island = {28,-4}
+Hawaiian_Islands = {34,-3.5}
+Phillipine_Islands = {18,-10}
+
+-- China
+Suiyuan = {9,5}
+Hupeh = {8,0}
+Fukien = {11,-2}
+Yunnan = {6,-3.5}
+
+-- SZs
+SZ2 = {-31,17}
+SZ4 = {-10,23}
+SZ5 = {-19,12}
+SZ6 = {-23,14}
+SZ7 = {-27,10}
+SZ9 = {-33,8}
+SZ10 = {-37,4}
+SZ12 = {-31,-2}
+SZ13 = {-25,0}
+SZ14 = {-19,-2}
+SZ35 = {0,-12}
+SZ41 = {26,-24}
+SZ44 = {42,-16}
+SZ50 = {19,-7}
+SZ51 = {26,-8}
+SZ53 = {19,-8}
+SZ56 = {37,3}
+SZ57 = {32,2}
+SZ61 = {17,2}
+SZ62 = {23,3}
+
+
+function setUp1941()
+    print("Setting year to 1941")
+
+    -- Set Up Armies
+
+    -- Axis
+    -- Italian Armies
+    -- Italy
+    placePiece(AAGBag,Italy,{0,0});
+    placePiece(ITA_TanBag,Italy,{1,-1});
+    placePiece(ITA_FigBag,Italy,{2,-2});
+    placePiece(ITA_InfBag,Italy,{2,-3});
+    placePiece(INDBag,Italy,{-1,-1});
+    -- Balkans - 2 Infantry,1 Tank
+    placePiece(ITA_InfBag,Balkans,{0,0});
+    placePiece(ITA_InfBag,Balkans,{0,-1});
+    placePiece(ITA_TanBag,Balkans,{-1,1});
+    -- Libya - 2 Infantry
+    placePiece(ITA_InfBag,Libya,{1,0});
+    placePiece(ITA_InfBag,Libya,{1,1});
+    -- SZ 14 - 1 Battleship,2 Cruisers,1 Transport
+    placePiece(ITA_BatBag,SZ14,{0,0});
+    placePiece(ITA_CruBag,SZ14,{1,-1});
+    placePiece(ITA_CruBag,SZ14,{0,2});
+    placePiece(ITA_TraBag,SZ14,{1,1});
+
+    -- Japanese Armies
+    -- Okinawa - 1 Infantry
+    placePiece(JPN_InfBag,Okinawa,{0,0});
+    -- Manchuria - 3 Infantry,1 Fighter
+    placePiece(JPN_InfBag,Manchuria,{0,0});
+    placePiece(JPN_InfBag,Manchuria,{1,0});
+    placePiece(JPN_InfBag,Manchuria,{.5,-1});
+    placePiece(JPN_FigBag,Manchuria,{-4,-2});
+    -- Japan - 1 Tank,1 Artillery,4 Infantry,1 Fighter,1 AA Gun,1 Ind Complex
+    placePiece(JPN_ArtBag,Japan,{0,0});
+    placePiece(Token3Bag,Japan,{-1,-1});
+    placePiece(JPN_InfBag,Japan,{-1,-1});
+    placePiece(JPN_FigBag,Japan,{1,2});
+    placePiece(AAGBag,Japan,{1,3});
+    placePiece(INDBag,Japan,{2,6});
+    -- SZ 62 - 1 Transport
+    placePiece(JPN_TraBag,SZ62,{0,0})
+    -- SZ 57 - 2 Aircraft Carrier,4 Fighter
+    placePiece(JPN_AirBag,SZ57,{0,0})
+    placePiece(JPN_AirBag,SZ57,{0,4})
+    -- Iwo Jima - 1 Infantry
+    placePiece(JPN_InfBag,Iwo_Jima,{0,0});
+    -- Caroline Islands - 3 Infantry
+    placePiece(Token1Bag,Caroline_Islands,{0,0});
+    placePiece(Token1Bag,Caroline_Islands,{0,0});
+    placePiece(JPN_InfBag,Caroline_Islands,{0,0});
+    -- SZ 51 - 1 Destroyer,2 Transports
+    placePiece(JPN_DesBag,SZ51,{0,0})
+    placePiece(JPN_TraBag,SZ51,{0,1})
+    placePiece(JPN_TraBag,SZ51,{0,2})
+    -- Formosa - 1 Fighter
+    placePiece(JPN_FigBag,Formosa,{0,0});
+    -- French Indo-China/Thailand - 3 Infantry
+    placePiece(Token1Bag,French_IndoChina_Thailand,{0,0});
+    placePiece(Token1Bag,French_IndoChina_Thailand,{0,0});
+    placePiece(JPN_InfBag,French_IndoChina_Thailand,{0,0});
+    -- SZ 61 - 1 Aircraft Carrier,2 Fighter,1 Battleship,1 Cruiser,1 Transport w/ 1 Infantry and 1 Artillery,1 Transport w/2 Infantry
+    placePiece(JPN_AirBag,SZ61,{0,0})
+    placePiece(JPN_BatBag,SZ61,{0,-1})
+    placePiece(JPN_CruBag,SZ61,{0,-2})
+    placePiece(JPN_TraBag,SZ61,{0,-3})
+    placePiece(JPN_InfBag,SZ61,{-.5,-3.5})
+    placePiece(JPN_ArtBag,SZ61,{.5,-3.5})
+    placePiece(JPN_TraBag,SZ61,{0,-5})
+    placePiece(JPN_InfBag,SZ61,{.5,-5.5})
+    placePiece(JPN_InfBag,SZ61,{-.5,-5.5})
+    -- Kiangsu - 3 Infantry
+    placePiece(Token1Bag,Kiangsu,{0,0});
+    placePiece(Token1Bag,Kiangsu,{0,0});
+    placePiece(JPN_InfBag,Kiangsu,{0,0});
+
+    -- German Armies
+    -- Northwestern Europe - 1 Fighter,1 Infantry
+    placePiece(GER_FigBag,NW_Europe,{0,0});
+    placePiece(GER_InfBag,NW_Europe,{1,1});
+    -- Germany - 3 Infantry,1 Fighter,1 Bomber,1 Artillery,1 AA Gun,1 Industrial Complex
+    placePiece(GER_InfBag,Germany,{0,0});
+    placePiece(GER_InfBag,Germany,{-1,0});
+    placePiece(GER_InfBag,Germany,{-1,-1});
+    placePiece(GER_FigBag,Germany,{0,-1});
+    placePiece(AAGBag,Germany,{1,0});
+    placePiece(INDBag,Germany,{0,1});
+    -- Norway - 2 Infantry,1 Fighter
+    placePiece(GER_InfBag,Norway,{0,0});
+    placePiece(GER_InfBag,Norway,{-1,0});
+    placePiece(GER_FigBag,Norway,{-1,-1});
+    -- SZ 5 - 1 Cruiser,1 Submarine,1 Transport
+    placePiece(GER_CruBag,SZ5,{0,0})
+    placePiece(GER_SubBag,SZ5,{1,2})
+    placePiece(GER_TraBag,SZ5,{2,4})
+    -- Finland - 2 Infantry
+    placePiece(GER_InfBag,Finland,{0,0});
+    placePiece(GER_InfBag,Finland,{0,3});
+    -- Poland - 4 Infantry,2 Tanks,1 Fighter,1 Artillery
+    placePiece(Token3Bag,Poland,{0,0});
+    placePiece(GER_InfBag,Poland,{0,0});
+    placePiece(GER_TanBag,Poland,{0,1});
+    placePiece(GER_TanBag,Poland,{-1,1});
+    placePiece(GER_ArtBag,Poland,{0,-1});
+    -- Czechoslovakia/Hungary - 2 Tanks
+    placePiece(GER_TanBag,Czechoslovakia_Hungary,{-.5,0});
+    placePiece(GER_TanBag,Czechoslovakia_Hungary,{.5,0});
+    -- Bulgaria/Romania - 3 Infantry,2 Tanks,1 Artillery
+    placePiece(Token1Bag,Bulgaria_Romania,{0,0});
+    placePiece(Token1Bag,Bulgaria_Romania,{0,0});
+    placePiece(GER_InfBag,Bulgaria_Romania,{0,0});
+    placePiece(GER_TanBag,Bulgaria_Romania,{1,1});
+    placePiece(GER_TanBag,Bulgaria_Romania,{0,1});
+    -- Libya - 1 Infantry,1 Artillery,1 Tank
+    placePiece(GER_InfBag,Libya,{0,0});
+    placePiece(GER_ArtBag,Libya,{-1,0});
+    placePiece(GER_TanBag,Libya,{-2,1});
+    -- Morocco/Algeria - 1 Infantry
+    placePiece(GER_InfBag,Morocco_Algeria,{0,0});
+    -- SZ 13 - 1 Transport
+    placePiece(GER_TraBag,SZ13,{0,0})
+    -- SZ 7 - 2 Submarines
+    placePiece(GER_SubBag,SZ7,{0,0})
+    placePiece(GER_SubBag,SZ7,{1,0})
+    -- France - 2 Infantry,1 Tank
+    placePiece(GER_InfBag,France,{0,0});
+    placePiece(GER_InfBag,France,{-1,-1});
+    placePiece(GER_TanBag,France,{-1,0});
+
+    -- Allies
+
+    -- Soviet Armies
+    -- Karelia S.S.R. 5 Infantry,1 Artillery,1 AA Gun,1 Industrial Complex
+    placePiece(Token1Bag,Karrelia_SSR,{0,0});
+    placePiece(Token3Bag,Karrelia_SSR,{0,0});
+    placePiece(SU_InfBag,Karrelia_SSR,{0,0});
+    placePiece(SU_ArtBag,Karrelia_SSR,{1,0});
+    placePiece(AAGBag,Karrelia_SSR,{0,-1});
+    placePiece(INDBag,Karrelia_SSR,{0,1});
+    -- SZ 4 - 1 Submarine
+    placePiece(SU_SubBag,SZ4,{0,0});
+    -- Archangel - 2 Infantry
+    placePiece(SU_InfBag,Archangel,{0,0});
+    placePiece(SU_InfBag,Archangel,{-1,-1});
+    -- Russia - 3 Infantry,1 Artillery,1 Tank,1 Industrial Complex,1 AA Gun
+    placePiece(SU_InfBag,Russia,{0,0});
+    placePiece(SU_InfBag,Russia,{1,0});
+    placePiece(SU_InfBag,Russia,{0,1});
+    placePiece(SU_ArtBag,Russia,{-1,-1});
+    placePiece(SU_TanBag,Russia,{1,1});
+    placePiece(INDBag,Russia,{-1,0});
+    placePiece(AAGBag,Russia,{-1,1});
+    -- Evenki National Okrug - 1 Infantry
+    placePiece(SU_InfBag,Evenki_National_Okrug,{0,0});
+    -- Yakut S.S.R. - 1 Infantry
+    placePiece(SU_InfBag,Yakut_SSR,{0,0});
+    -- Stanovoj Chrebet - 2 Infantry
+    placePiece(SU_InfBag,Stanovoj_Chrebet,{0,0});
+    placePiece(SU_InfBag,Stanovoj_Chrebet,{1,0});
+    -- Soviet Far East - 2 Infantry
+    placePiece(SU_InfBag,Soviet_Far_East,{0,0});
+    placePiece(SU_InfBag,Soviet_Far_East,{1,0});
+    -- Buryatia_SSR S.S.R. - 3 Infantry
+    placePiece(SU_InfBag,Buryatia_SSR,{0,0});
+    placePiece(SU_InfBag,Buryatia_SSR,{-1,0});
+    placePiece(SU_InfBag,Buryatia_SSR,{0,-1});
+    -- Novosibirsk - 2 Infantry
+    placePiece(SU_InfBag,Novosibirsk,{0,0});
+    placePiece(SU_InfBag,Novosibirsk,{0,1});
+    -- Kazakh S.S.R. - 2 Infantry
+    placePiece(SU_InfBag,Kazakh_SSR,{0,0});
+    placePiece(SU_InfBag,Kazakh_SSR,{-1,0});
+    -- Caucasus - 4 Infantry,1 AA Gun,1 Industrial Complex
+    placePiece(SU_InfBag,Caucasus,{0,0});
+    placePiece(SU_InfBag,Caucasus,{1,0});
+    placePiece(SU_InfBag,Caucasus,{1,-1});
+    placePiece(SU_InfBag,Caucasus,{0,-1});
+    placePiece(AAGBag,Caucasus,{1,1});
+    placePiece(INDBag,Caucasus,{2,1});
+    -- Eastern Ukraine - 1 Infantry
+    placePiece(SU_InfBag,E_Ukraine,{0,0});
+    -- Ukraine - 2 Infantry
+    placePiece(SU_InfBag,Ukraine,{0,0});
+    placePiece(SU_InfBag,Ukraine,{1,0});
+    -- Eastern Poland - 2 Infantry
+    placePiece(SU_InfBag,E_Poland,{0,0});
+    placePiece(SU_InfBag,E_Poland,{1,0});
+    -- Belorussia - 2 Infantry
+    placePiece(SU_InfBag,Belorussia,{0,0});
+    placePiece(SU_InfBag,Belorussia,{0,1});
+    -- Baltic States - 3 Infantry
+    placePiece(SU_InfBag,Baltic_States,{0,0});
+    placePiece(SU_InfBag,Baltic_States,{-1,0});
+    placePiece(SU_InfBag,Baltic_States,{1,0});
+
+    -- UK Armies
+    -- SZ 9 - 1 Destroyer,1 Transport
+    placePiece(UK_DesBag,SZ9,{0,0});
+    placePiece(UK_TraBag,SZ9,{1,0});
+    -- SZ 2 - 1 Battleship,1 Transport
+    placePiece(UK_BatBag,SZ2,{0,0});
+    placePiece(UK_TraBag,SZ2,{1,0});
+    -- UK - 2 Infantry,1 Artillery,1 Tank,2 Fighters,1 Bomber,1 AA Gun,1 Ind. Complex
+    placePiece(Token1Bag,UK,{0,0});
+    placePiece(UK_InfBag,UK,{0,0});
+    placePiece(UK_ArtBag,UK,{-1,1});
+    placePiece(Token1Bag,UK,{0,1});
+    placePiece(UK_TanBag,UK,{0,1});
+    placePiece(UK_FigBag,UK,{-1,2});
+    placePiece(UK_FigBag,UK,{-3,3});
+    placePiece(UK_BomBag,UK,{-2,4});
+    placePiece(AAGBag,UK,{1,0});
+    placePiece(INDBag,UK,{1,1});
+    -- SZ 6 - 1 Destroyer
+    placePiece(UK_DesBag,SZ6,{0,0});
+    -- W. Canada - 1 Infantry
+    placePiece(UK_InfBag,W_Canada,{0,0});
+    -- Kwangtung - 1 Infantry
+    placePiece(UK_InfBag,Kwangtung,{0,0});
+    -- SZ 41 - 1 Destroyer,1 Transport
+    placePiece(UK_DesBag,SZ41,{0,0});
+    placePiece(UK_TraBag,SZ41,{1,0});
+    -- Australia - 3 Infantry,1 Artillery,1 AA Gun
+    placePiece(UK_InfBag,Australia,{0,0});
+    placePiece(UK_InfBag,Australia,{1,0});
+    placePiece(UK_InfBag,Australia,{-1,0});
+    placePiece(UK_ArtBag,Australia,{1,1});
+    placePiece(AAGBag,Australia,{0,1});
+    -- India - 3 Infantry,1 Artillery,1 AA Gun
+    placePiece(UK_InfBag,India,{0,0});
+    placePiece(UK_InfBag,India,{1,0});
+    placePiece(UK_InfBag,India,{0,1});
+    placePiece(UK_ArtBag,India,{1,1});
+    placePiece(AAGBag,India,{0,-1});
+    -- SZ 35 - 1 Destroyer,1 Transport
+    placePiece(UK_DesBag,SZ35,{0,0});
+    placePiece(UK_TraBag,SZ35,{-1,0});
+    -- Trans-Jordan - 2 Infantry
+    placePiece(UK_InfBag,TransJordan,{0,0});
+    placePiece(UK_InfBag,TransJordan,{1,0});
+    -- Union of S. Africa - 2 Infantry
+    placePiece(UK_InfBag,Union_of_S_Africa,{0,0});
+    placePiece(UK_InfBag,Union_of_S_Africa,{0,1});
+    -- Egypt - 2 Infantry,1 Tank,1 Fighter,1 Artillery
+    placePiece(Token1Bag,Egypt,{0,0});
+    placePiece(UK_InfBag,Egypt,{0,0});
+    placePiece(UK_TanBag,Egypt,{0,1});
+    placePiece(UK_ArtBag,Egypt,{1,0});
+    -- SZ 12: 1 Destroyer,1 Cruiser
+    placePiece(UK_DesBag,SZ12,{0,0});
+    placePiece(UK_CruBag,SZ12,{1,0});
+    -- E. Canada - 1 Tank
+    placePiece(UK_TanBag,E_Canada,{0,0});
+    -- Burma - 1 Infantry
+    placePiece(UK_InfBag,Burma,{0,0});
+
+    -- US Armies
+    -- E. US - 1 Infantry,1 Fighter,1 Bomber,1 Artillery,1 AA Gun,1 Ind. Complex
+    placePiece(US_InfBag,E_US,{0,0});
+    placePiece(US_FigBag,E_US,{0,1});
+    placePiece(US_FigBag,E_US,{1,2});
+    placePiece(AAGBag,E_US,{1,0});
+    placePiece(INDBag,E_US,{1,-1});
+    -- SZ 10 - 1 Transport,1 Destroyer
+    placePiece(US_DesBag,SZ10,{0,0});
+    placePiece(US_TraBag,SZ10,{0,-1});
+    -- Alaska - 1 Infantry
+    placePiece(US_InfBag,Alaska,{0,0});
+    -- C. US - 1 Infantry
+    placePiece(US_InfBag,C_US,{0,0});
+    -- W. US - 1 Infantry,1 Fighter,1 Bomber,1 AA Gun,1 Ind. Complex
+    placePiece(US_InfBag,W_US,{0,0});
+    placePiece(US_FigBag,W_US,{0,1});
+    placePiece(US_BomBag,W_US,{0,-1});
+    placePiece(AAGBag,W_US,{-1,0});
+    placePiece(INDBag,W_US,{-1,1});
+    -- SZ 56 - 1 Transport,1 Destroyer
+    placePiece(US_DesBag,SZ56,{0,0});
+    placePiece(US_TraBag,SZ56,{0,1});
+    -- Midway - 1 Infantry
+    placePiece(US_InfBag,Midway,{0,0});
+    -- Wake Island - 1 Infantry
+    placePiece(US_InfBag,Wake_Island,{0,0});
+    -- Hawaiian Islands - 1 Infantry,1 Fighter
+    placePiece(US_InfBag,Hawaiian_Islands,{0,0});
+    placePiece(US_FigBag,Hawaiian_Islands,{-1,1});
+    -- SZ 53: 1 Battleship
+    placePiece(US_BatBag,SZ53,{0,0});
+    -- SZ 44: 1 Aircraft Carrier,1 Fighter,1 Destroyer
+    placePiece(US_AirBag,SZ44,{0,0});
+    placePiece(US_DesBag,SZ44,{0,1});
+    -- Phillipine Islands - 2 Infantry
+    placePiece(US_InfBag,Phillipine_Islands,{0,0});
+    placePiece(US_InfBag,Phillipine_Islands,{0,1});
+    -- SZ 50: 1 Transport,1 Destroyer
+    placePiece(US_DesBag,SZ50,{0,0});
+    placePiece(US_TraBag,SZ50,{0,1});
+    -- China
+    -- Suiyuan - 1 Infantry
+    placePiece(CH_InfBag,Suiyuan,{0,0});
+    -- Hupeh - 1 Infantry
+    placePiece(CH_InfBag,Hupeh,{0,0});
+    -- Fukien - 1 Infantry
+    placePiece(CH_InfBag,Fukien,{0,0});
+    -- Yunnan - 1 Infantry,1 Fighter
+    placePiece(CH_InfBag,Yunnan,{0,0});
+    placePiece(US_FigBag,Yunnan,{1,1});
+
+    zOffset = zOffset + 1
+    --Carried Fighters
+    -- SZ 57 - 2 Aircraft Carrier,4 Fighter
+    placePiece(JPN_FigBag,SZ57,{1,0})
+    placePiece(JPN_FigBag,SZ57,{-1,0})
+    placePiece(JPN_FigBag,SZ57,{1,4})
+    placePiece(JPN_FigBag,SZ57,{-1,4})
+    -- SZ 61 - 1 Aircraft Carrier,2 Fighter,1 Battleship,1 Cruiser,1 Transport w/ 1 Infantry and 1 Artillery,1 Transport w/2 Infantry
+    placePiece(JPN_FigBag,SZ61,{-1,0})
+    placePiece(JPN_FigBag,SZ61,{1,0})
+    -- SZ 44: 1 Aircraft Carrier,1 Fighter,1 Destroyer
+    placePiece(US_FigBag,SZ44,{-1,0});
+
+    zOffset = zOffset - 1
+
+end
+
+function placePiece(bag,location,offset)
+    bag.takeObject({position = {location[1] + offset[1],zOffset,location[2] + offset[2]}});
+    print(location[1] .. "," .. location[2])
+end
